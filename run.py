03563fd3720bfe8ecc60e3c5d2335074138bd57f
anastasia <anast.korotkova2018@yandex.ru>
2023-03-22 19:42:24 +0300

    feat: add handlers for the help command and info commands bot

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from tg_bot.config import load_config
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram.filters import Command
 from aiogram.types import Message
+import tg_bot.bot_responses.text as text_responses
 
 
 config = load_config('.env')
@@ -9,10 +10,22 @@ bot: Bot = Bot(config.tg_bot.token)
 dp: Dispatcher = Dispatcher()
 
 
-# хендлер для команды /start, тестовый запуск
-@dp.message(CommandStart())
-async def process_start_command(message: Message):
-    await message.answer('Тестовый запуск')
+# хендлер для команды /start и /help
+@dp.message(Command(commands=['start', 'help']))
+async def process_start_and_help_cmd(message: Message):
+    await message.answer(text_responses.START_AND_HELP_TEXT, parse_mode='HTML')
+
+
+# хендлер для команды !command_admin_bot
+@dp.message(Command('command_admin_bot', prefix='!'))
+async def process_command_admin_bot_cmd(message: Message):
+    await message.answer(text_responses.COMMAND_ADMIN_BOT_TEXT)
+
+
+# хендлер для команды !about_admin_bot
+@dp.message(Command('about_admin_bot', prefix='!'))
+async def process_about_admin_bot_cmd(message: Message):
+    await message.reply(text_responses.ABOUT_ADMIN_BOT_TEXT)
 
 
 if __name__ == '__main__':
---
+++ tg_bot/bot_responses/text.py
@@ -0,0 +1,9 @@
+START_AND_HELP_TEXT = '''Привет. Я - бот для помощи с модерацией чата.
+Добавь меня в группу и сделай админом.
+<code>!command_admin_bot</code> - справка о моих командах
+<code>!about_admin_bot</code> - мой исходный код
+'''
+
+COMMAND_ADMIN_BOT_TEXT = 'Список команд для настройки бота'
+
+ABOUT_ADMIN_BOT_TEXT = 'https://github.com/poikl246/telegram-admin-bot'
