891278f52c6a731c2e27a74d96d8036dca75131d
Nikita <poikl.m246@gmail.com>
2023-04-09 21:50:26 +0500

    Добавление чата + добавление сообщения по тегу (но тут еще нужно будет подумать)

--- tg_bot/database/DB.py
+++ tg_bot/database/DB.py
@@ -3,17 +3,25 @@ import time
 
 import asyncpg
 
+import tracemalloc
+tracemalloc.start()
 
-class Database:
-    _instance = None
+class Singleton(type):
+    _instances = {}
 
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-    def __init__(self):
-        print(123)
+
+
+class Database(metaclass=Singleton):
+
+
+
+    # def __init__(self):
+    #     print(123)
 
     async def connect(self, user, password, database, host):
         self.db = await asyncpg.connect(user=user, password=password,
@@ -92,13 +100,18 @@ class Database:
 
         await self.db.execute('''
         CREATE TABLE IF NOT EXISTS speed_message (
+                id SERIAL PRIMARY KEY,
                 tag TEXT NOT NULL,
-                id_chat BIGINT NOT NULL REFERENCES chats(id_chat),
-                message TEXT NOT NULL,
-                PRIMARY KEY (tag, id_chat)
+                id_chat INTEGER NOT NULL REFERENCES chats(id_chat),
+                message TEXT NOT NULL
             );''')
 
+    async def put_new_chat(self, id_chat: int, name: str):
 
+        await self.db.execute("INSERT INTO chats (id_chat, name) VALUES ($1, $2)", id_chat, name)
+
+    async def put_speed_message(self, tag: str, id_chat: int, message: str):
+        await self.db.execute("INSERT INTO speed_message (tag, id_chat, message) VALUES ($1, $2, $3);", tag, id_chat, message)
 
 
     async def drop_all_table(self):
@@ -107,24 +120,33 @@ class Database:
 
 
 async def main():
-    db = Database()
-    await db.connect(user='user', password='LlGMTNj3qux5YYoooNLX', host='172.17.0.2', database='mydatabase')
-    await db.create_tables()
+    # Пример использования)
+
+    db = Database() # Инициализация. Делаем для подключения или если нужно обратиться к бд
+
+    await db.connect(user='user', password='LlGMTNj3qux5YYoooNLX', host='localhost', database='mydatabase') # Подключение к базе. Нужно сделать один раз. Дальше можно просто юзать Database().
+    # Данные для авторизации естественно берем свои
+    # локально базу поднять можно в докере.
+    # docker run --rm --name selectel-pgdocker -e POSTGRES_PASSWORD=LlGMTNj3qux5YYoooNLX -e POSTGRES_USER=user -e POSTGRES_DB=mydatabase -d -p 5432:5432  postgres
+
+    await db.create_tables() # Создаем таблицы. Тут есть проверка на то что таблицы уже существуют. Так что можно просто вызвать при запуске бота
 
 
-    db1 = Database()
+
+
+    db1 = Database() # собственно проверка)
+    print(id(db), id(db1))
+
+    await db1.put_new_chat(1, 'test') # Чат можно добавить только один раз. Дальше будет ошибка. Потом придумаем что сделать и как правильно писать их
+
+    await db1.put_speed_message('джун', 1, '123') # Так добавляем сообщение
 
 
 
 
-    print(db, db1)
-
-    await asyncio.sleep(2)
-
-    await db1.drop_all_table()
-
+    # await db1.drop_all_table() # Удаляет все таблицы и все что в них могло быть. Так что для тестов норм.
     await db1.close()
 
 
-
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
