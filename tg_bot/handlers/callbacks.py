c4a8fb098f99b64546493c058b486379705873f0
anastasia <anast.korotkova2018@yandex.ru>
2023-04-27 22:33:32 +0300

    fix: fix buttons

--- tg_bot/handlers/callbacks.py
+++ tg_bot/handlers/callbacks.py
@@ -1,16 +1,25 @@
-from aiogram import Router
+from aiogram import Bot, Router
 from aiogram.types import CallbackQuery
 
 from tg_bot.handlers.callbacks_data import SettingCallback
+from tg_bot.cache import admin_cache
+from tg_bot.utils.utils import get_admins_id_set
 
 
 router: Router = Router()
 
 
 @router.callback_query(SettingCallback.filter())
-async def callback_settings(callback: CallbackQuery, callback_data: SettingCallback):
-    if callback_data.flag == 'on':
-        await callback.answer(text="Включено", show_alert=True)
-    elif callback_data.flag == 'of':
-        await callback.answer(text="Отключено", show_alert=True)
+async def callback_settings(callback: CallbackQuery, callback_data: SettingCallback, bot: Bot):
+    chat_id = callback.message.chat.id
+    admins = admin_cache.get(chat_id)
+    if not admins:
+        admin_cache[callback.message.chat.id] = await get_admins_id_set(chat_id=chat_id, bot=bot)
+    if callback.from_user.id in admins:
+        if callback_data.flag == 'on':
+            await callback.answer(text="Включено", show_alert=True)
+        elif callback_data.flag == 'of':
+            await callback.answer(text="Отключено", show_alert=True)
+    else:
+        await callback.answer(text="Доступно только админам", show_alert=True)
     await callback.answer()
