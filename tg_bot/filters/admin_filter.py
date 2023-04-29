c582bb3230eb52d82cfc4d6d0516576da1f168a8
anastasia <anast.korotkova2018@yandex.ru>
2023-04-29 16:40:26 +0300

    fix: remove parsing admins in private chat

--- tg_bot/filters/admin_filter.py
+++ tg_bot/filters/admin_filter.py
@@ -9,17 +9,19 @@ from tg_bot.utils.utils import get_admins_id_set
 
 @dataclass
 class IsAdmin(BaseFilter):
-    def get_user_id(self, message: types.Message, bot: Bot) -> int:
+    def _get_user_id(self, message: types.Message) -> int:
         return message.from_user.id
     
-    async def _get_chat_member(self, message: types.Message, bot: Bot):    
+    async def _get_chat_member(self, message: types.Message, bot: Bot): 
+        if message.chat.type == 'private':
+            return True
         admins = admin_cache.get(message.chat.id)
         if not admins:
             admins = await get_admins_id_set(chat_id=message.chat.id, bot=bot)
-        target_user_id = self.get_user_id(message, bot)
+        target_user_id = self._get_user_id(message)
         if target_user_id in admins:
             return True
-        return False
+        return False 
     
     async def __call__(self, message: types.Message, bot: Bot):
         chat_member = await self._get_chat_member(message, bot)
--- tg_bot/handlers/callbacks.py
+++ tg_bot/handlers/callbacks.py
@@ -11,15 +11,18 @@ router: Router = Router()
 
 @router.callback_query(SettingCallback.filter())
 async def callback_settings(callback: CallbackQuery, callback_data: SettingCallback, bot: Bot):
-    chat_id = callback.message.chat.id
-    admins = admin_cache.get(chat_id)
-    if not admins:
-        admin_cache[callback.message.chat.id] = await get_admins_id_set(chat_id=chat_id, bot=bot)
-    if callback.from_user.id in admins:
-        if callback_data.flag == 'on':
-            await callback.answer(text="Включено", show_alert=True)
-        elif callback_data.flag == 'of':
-            await callback.answer(text="Отключено", show_alert=True)
+    if callback.message.chat.type == 'private':
+        await callback.answer(text="Вы используете команду в приватном чате", show_alert=True)
     else:
-        await callback.answer(text="Доступно только админам", show_alert=True)
+        chat_id = callback.message.chat.id
+        admins = admin_cache.get(chat_id)
+        if not admins:
+            admin_cache[callback.message.chat.id] = await get_admins_id_set(chat_id=chat_id, bot=bot)
+        if callback.from_user.id in admins:
+            if callback_data.flag == 'on':
+                await callback.answer(text="Включено", show_alert=True)
+            elif callback_data.flag == 'of':
+                await callback.answer(text="Отключено", show_alert=True)
+        else:
+            await callback.answer(text="Доступно только админам", show_alert=True)
     await callback.answer()
