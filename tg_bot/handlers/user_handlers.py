315462c37a64d0d9a032f32cf2f42abb5ebab917
anastasia <anast.korotkova2018@yandex.ru>
2023-04-30 19:44:01 +0300

    refactor: rename commands

--- tg_bot/handlers/user_handlers.py
+++ tg_bot/handlers/user_handlers.py
@@ -14,13 +14,13 @@ async def process_start_and_help_cmd(message: Message):
     await message.answer(text.START_AND_HELP_TEXT, parse_mode='HTML')
 
 
-@router.message(Command(commands=['command_admin_bot']))
-async def process_command_admin_bot_cmd(message: Message):
+@router.message(Command(commands=['command']))
+async def process_command_cmd(message: Message):
     await message.answer(text.COMMAND_ADMIN_BOT_TEXT, parse_mode='HTML')
 
 
-@router.message(Command(commands=['about_admin_bot']))
-async def process_about_admin_bot_cmd(message: Message):
+@router.message(Command(commands=['about']))
+async def process_about_cmd(message: Message):
     await message.reply(text.ABOUT_ADMIN_BOT_TEXT)
 
 
