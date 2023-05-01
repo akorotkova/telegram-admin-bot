ccf4527edb358249f741c847ed127f7eb2fcb0f4
anastasia <anast.korotkova2018@yandex.ru>
2023-05-01 14:46:12 +0300

    feat: add removing circles

--- tg_bot/handlers/bot_handlers.py
+++ tg_bot/handlers/bot_handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 from aiogram.methods.delete_message import DeleteMessage
 
 
@@ -12,7 +12,7 @@ async def process_warning_voice(message: Message):
     return DeleteMessage(chat_id=message.chat.id, message_id=message.message_id)
 
 
-@router.message(F.video)
+@router.message(F.content_type.in_({ContentType.VIDEO, ContentType.VIDEO_NOTE}))
 async def process_warning_voice(message: Message):
     await message.reply(text='Пожалуйста, не отправляйте видеосообщения в чат.')
     return DeleteMessage(chat_id=message.chat.id, message_id=message.message_id)
