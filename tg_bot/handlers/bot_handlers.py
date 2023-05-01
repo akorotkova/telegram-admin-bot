b442c9b5dbd5346df7ad4e65446a1aac758dcb91
anastasia <anast.korotkova2018@yandex.ru>
2023-05-01 13:47:52 +0300

    feat: add delete voice and video messages

--- tg_bot/handlers/bot_handlers.py
+++ tg_bot/handlers/bot_handlers.py
@@ -1,5 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import Message
+from aiogram.methods.delete_message import DeleteMessage
 
 
 router: Router = Router()
@@ -8,3 +9,10 @@ router: Router = Router()
 @router.message(F.voice)
 async def process_warning_voice(message: Message):
     await message.reply(text='Пожалуйста, не отправляйте голосовые сообщения в чат.')
+    return DeleteMessage(chat_id=message.chat.id, message_id=message.message_id)
+
+
+@router.message(F.video)
+async def process_warning_voice(message: Message):
+    await message.reply(text='Пожалуйста, не отправляйте видеосообщения в чат.')
+    return DeleteMessage(chat_id=message.chat.id, message_id=message.message_id)
