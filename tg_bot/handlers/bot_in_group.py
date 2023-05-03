5ee8e14d66fac7cd3c187c8de39f9eaa2b2f501b
anastasia <anast.korotkova2018@yandex.ru>
2023-05-03 19:47:40 +0300

    feat: add check that the bot a administrator

--- tg_bot/handlers/bot_in_group.py
+++ tg_bot/handlers/bot_in_group.py
@@ -9,8 +9,8 @@ from aiogram.types import ChatMemberUpdated
 
 
 router: Router = Router()
-router.my_chat_member.filter(F.chat.type.in_({"group", "supergroup"}))
-chats_variants = {"group": "группу", "supergroup": "супергруппу"}
+router.my_chat_member.filter(F.chat.type.in_({'group', 'supergroup'}))
+chats_variants = {'group': 'группу', 'supergroup': 'супергруппу'}
 
 
 @router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=IS_NOT_MEMBER >> MEMBER))
@@ -19,23 +19,49 @@ async def bot_added_as_member(event: ChatMemberUpdated, bot: Bot):
     if chat_info.permissions.can_send_messages:
         await bot.send_message(
             chat_id=event.chat.id,
-            text=f"Привет! Вы добавили меня в "
+            text=f'Привет! Вы добавили меня в '
                  f'{chats_variants[event.chat.type]} "{event.chat.title}" '
-                 f"как обычного участника. ID чата: {event.chat.id}\n"
-                 f'Чтобы я мог помогать с модерацией чата, добавьте меня в админы.'
+                 f'как обычного участника.\n'
+                 f'Чтобы я мог помогать вам с модерацией чата, добавьте меня в админы.'
         )
     else:
-        print("логирование этой ситуации")
+        print('логирование этой ситуации')
 
 
 @router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=MEMBER >> ADMINISTRATOR))
 async def bot_added_as_admin(event: ChatMemberUpdated, bot: Bot):
-    await bot.send_message(
+    chat_member = event.new_chat_member
+    if not chat_member.can_delete_messages:
+        await bot.send_message(
         chat_id=event.chat.id,
-        text=f"Спасибо, что добавили меня в "
-             f'{chats_variants[event.chat.type]} "{event.chat.title}" '
-             f"как администратора. ID чата: {event.chat.id}\n"
-             f'Теперь я буду помогать вам с модерацией. Перейдите в настройки: /command'
+        text=f'Спасибо, что добавили меня '
+             f'в администраторы, но мне нужны все права админа, '
+             f'чтобы я мог помогать с модерацией!\n'
+             f'Пожалуйста, добавьте мне право на удаление сообщений в чате.'
+        )
+    else:        
+        await bot.send_message(
+            chat_id=event.chat.id,
+            text=f'Спасибо, что добавили меня в администраторы! '
+                 f'Теперь я буду помогать вам с модерацией.\n'
+                 f'Перейдите в настройки: /command'
+            )
+
+
+@router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=ADMINISTRATOR))
+async def bot_restricted_admin(event: ChatMemberUpdated, bot: Bot):
+    chat_member = event.new_chat_member
+    if not chat_member.can_delete_messages:
+        await bot.send_message(
+            chat_id=event.chat.id,
+            text=f'Вы ограничили мне права.\n'
+                 f'Чтобы я мог помогать вам с модерацией, мне нужны все права админа.'
+    )
+    else:
+        await bot.send_message(
+            chat_id=event.chat.id,
+            text=f'Спасибо! Теперь я буду помогать вам с модерацией.\n'
+                 f'Перейдите в настройки: /command'
         )
 
 
@@ -43,5 +69,6 @@ async def bot_added_as_admin(event: ChatMemberUpdated, bot: Bot):
 async def bot_added_as_admin(event: ChatMemberUpdated, bot: Bot):
     await bot.send_message(
         chat_id=event.chat.id,
-        text=f"Вы удалили бот из администраторов чата.\nЧтобы бот мог помогать с модерацией, ему нужны права админа."
+        text=f'Вы удалили меня из администраторов чата.\n'
+             f'Чтобы я мог помогать вам с модерацией, мне нужны права админа.'
     )
