976a0c46d369af006a34751559e76bdda5d47cb1
anastasia <anast.korotkova2018@yandex.ru>
2023-05-06 15:22:54 +0300

    refactor: change responses in private chat

--- tg_bot/handlers/bot_handlers.py
+++ tg_bot/handlers/bot_handlers.py
@@ -11,16 +11,16 @@ router: Router = Router()
 def check_bot_permissions(handler):
     @wraps(handler)
     async def inner(message: Message, bot: Bot):
+        if message.chat.type == 'private':
+            return await handler(message, bot)
         bot_info = await bot.get_chat_member(message.chat.id, bot.id)
-        # если включена настройка и есть права (берем из базы)
         if 'включена настройка' and bot_info.can_delete_messages:
             return await handler(message, bot)
-        # если включена настройка, но нет прав (берем из базы)
         if 'включена настройка' and not bot_info.can_delete_messages:
             return await bot.send_message(
                 chat_id=message.chat.id,
                 text=f'Вы включили настройку автоудаления, но у меня нет прав на это. '
-                     f'Разрешите мне удалять сообщения в чате.'
+                     f'Проверьте, являюсь ли я админом чата с возможностью удалять сообщения.'
                 )
     return inner
   
