f9bdbf58358ef0dd2d274a4eff526f5592cf2485
anastasia <anast.korotkova2018@yandex.ru>
2023-05-12 16:20:10 +0300

    refactor: change appearance of the code

--- tg_bot/handlers/bot_handlers.py
+++ tg_bot/handlers/bot_handlers.py
@@ -1,50 +1,57 @@
 from functools import wraps
 
-from aiogram import Bot, Router, F
-from aiogram.types import Message, ContentType
+from aiogram import Bot, Router, types, F
 from aiogram.methods.delete_message import DeleteMessage
 
 
-router: Router = Router()
+router = Router()
+
+_allowed_formats_file = {'pdf', 'txt', 'conf', 'json', 'xml', 'yml', 'csv', 'png', 'jpg', 'jpeg'}
 
 
-def check_bot_permissions(handler):
+def check_bot_permission_to_delete(handler):
     @wraps(handler)
-    async def inner(message: Message, bot: Bot):
+    async def inner(message: types.Message, bot: Bot):
+
         if message.chat.type == 'private':
             return await handler(message, bot)
+
         bot_info = await bot.get_chat_member(message.chat.id, bot.id)
-        if 'включена настройка' and bot_info.can_delete_messages:
-            return await handler(message, bot)
-        if 'включена настройка' and not bot_info.can_delete_messages:
+
+        permission_to_delete: bool = bot_info.can_delete_messages
+        config_value: bool = True  # TODO берем из базы значение настройки
+
+        if config_value and not permission_to_delete:
             return await bot.send_message(
                 chat_id=message.chat.id,
-                text=f'Вы включили настройку автоудаления, но у меня нет прав на это. '
-                     f'Проверьте, являюсь ли я админом чата с возможностью удалять сообщения.'
-                )
+                text='Вы включили настройку автоудаления, но у меня нет прав на это. '
+                     'Проверьте, являюсь ли я админом чата с возможностью удалять сообщения.'
+            )
+        if config_value and permission_to_delete:
+            return await handler(message, bot) 
+        
     return inner
 
 
-@router.message(F.content_type == ContentType.VOICE)
-@check_bot_permissions
-async def process_warning_voice(message: Message, bot: Bot):
+@router.message(F.content_type == types.ContentType.VOICE)
+@check_bot_permission_to_delete
+async def process_deleting_voice(message: types.Message, bot: Bot):
     await message.reply(text='Пожалуйста, не отправляйте голосовые сообщения в чат.')
     return DeleteMessage(chat_id=message.chat.id, message_id=message.message_id)
     
 
-@router.message(F.content_type.in_({ContentType.VIDEO, ContentType.VIDEO_NOTE}))
-@check_bot_permissions
-async def process_warning_voice(message: Message, bot: Bot):
+@router.message(F.content_type.in_({types.ContentType.VIDEO, types.ContentType.VIDEO_NOTE}))
+@check_bot_permission_to_delete
+async def process_deleting_video(message: types.Message, bot: Bot):
     await message.reply(text='Пожалуйста, не отправляйте видеосообщения в чат.')
-    return DeleteMessage(chat_id=message.chat.id, message_id=message.message_id)
+    return DeleteMessage(chat_id=message.chat.id,message_id=message.message_id)
 
 
-@router.message(F.content_type.in_({ContentType.DOCUMENT}))
-@check_bot_permissions
-async def process_warning_document(message: Message, bot: Bot):
-    _allowed_formats_file = {'pdf', 'txt', 'conf', 'json', 'xml', 'yml', 'csv', 'png', 'jpg', 'jpeg'}
-    file_format = message.document.file_name.split('.')[-1]
-    if file_format not in _allowed_formats_file:
+@router.message(F.content_type == types.ContentType.DOCUMENT)
+@check_bot_permission_to_delete
+async def process_deleting_document(message: types.Message, bot: Bot):
+    format_file = message.document.file_name.split('.')[-1]
+    if format_file not in _allowed_formats_file:
         await message.reply(text='Запрещено отправлять в чат файл с данным расширением.')
         return DeleteMessage(chat_id=message.chat.id, message_id=message.message_id)
         
